6d1f8211868c57fbf4750bf349a1cb1ab6f53582
Alexander Cheung <cheung.ying.lon@gmail.com>
2017-08-26 16:18:33 -0500

    display image placeholder if photo does not exist

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,7 +1,7 @@
 module ItemsHelper
 	# Return item photo or camera icon
 	def item_photo(item)
-		if item.photo
+		if item.photo && item.photo != ""
 			return "#{filepicker_image_tag item.photo, fit: 'crop', w: 200, h: 200}"
 		else
 			return "#{fa_icon 'camera'}"
